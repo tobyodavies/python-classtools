058036e2b55bc1fdbf9ee1c41924f7216f2cbf0d
Toby Davies <Toby Davies tobyodavies@gmail.com>
2012-10-11 21:00:49 +1100

    Setuptools support

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='classtools',
+            version='0.1',
+            description='Functools for classes',
+            author='Toby Davies',
+            author_email='tobyodavies@gmail.com',
+            url='https://github.com/tobyodavies/python-classtools/',
+            packages=['classtools'],
+           )
