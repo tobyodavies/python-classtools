d99a31e24377c144c64c0dd54c240f8c0a389791
Toby Davies <Toby Davies tobyodavies@gmail.com>
2012-10-11 17:53:59 +1100

    Cleaned up documentation

--- __init__.py
+++ __init__.py
@@ -1,36 +1,42 @@
 """
-A descriptor for instance-based inner classes
+This library aims to be a simple set of decorators and tools for use with classes.
 
->>> class foo(object):
-...     @innerclass
-...     class bar(object):
-...         "This is a Bar"
-...         def __init__(self, outer):
-...             self.parentinstance = outer
-...
->>> f = foo()
->>> b = f.bar()
->>> b.__class__.__name__
-'bar'
->>> b.__doc__
-'This is a Bar'
->>> b.__module__ == f.__module__
-True
->>> b.parentinstance is f
-True
->>> b2 = f.bar()
->>> b2.__class__ == b.__class__
-True
->>> f2 = foo()
->>> b3 = f2.bar()
->>> b3.parentinstance == b.parentinstance
-False
+Functools works remarkably well for a lot of things
+but there are some things that ought to be easier to do.
 """
 
 import functools
 
 class innerclass(object):
-
+    """
+    A descriptor for instance-based inner classes
+    
+    >>> class foo(object):
+    ...     @innerclass
+    ...     class bar(object):
+    ...         "This is a Bar"
+    ...         def __init__(self, outer):
+    ...             self.parentinstance = outer
+    ...
+    >>> f = foo()
+    >>> b = f.bar()
+    >>> b.__class__.__name__
+    'bar'
+    >>> b.__doc__
+    'This is a Bar'
+    >>> b.__module__ == f.__module__
+    True
+    >>> b.parentinstance is f
+    True
+    >>> b2 = f.bar()
+    >>> b2.__class__ == b.__class__
+    True
+    >>> f2 = foo()
+    >>> b3 = f2.bar()
+    >>> b3.parentinstance == b.parentinstance
+    False
+    """
+    
     def __init__(self, klass):
         self.klass = klass
 
